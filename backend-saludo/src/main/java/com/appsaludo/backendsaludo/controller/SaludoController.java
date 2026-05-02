89e0f1b979917a3c7120e5d74a5ca10d18216208
Jchuquin <jechuquine@ejercito.mil.ec>
2026-05-01 22:55:57 -0500

    fix 5

--- backend-saludo/src/main/java/com/appsaludo/backendsaludo/controller/SaludoController.java
+++ backend-saludo/src/main/java/com/appsaludo/backendsaludo/controller/SaludoController.java
@@ -10,6 +10,6 @@ public class SaludoController {
 
   @GetMapping("/saludo")
   public String obtenerSaludo() {
-    return "Hola, bienvenido a la aplicación de saludo! 4";
+    return "Hola, bienvenido a la aplicación de saludo! 5";
   }
 }
