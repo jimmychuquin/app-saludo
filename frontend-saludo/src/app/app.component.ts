8bfa7881c52a2f9b04bdd2cd51f94322e9627ec2
Jchuquin <jechuquine@ejercito.mil.ec>
2026-05-01 20:56:57 -0500

    deploy test2

--- frontend-saludo/src/app/app.component.ts
+++ frontend-saludo/src/app/app.component.ts
@@ -10,8 +10,8 @@ import { getAccessToken, logout } from './auth';
   template: `
     <main class="page">
       <section class="card">
-        <h1>Prueba Saludo Implementacion </h1>
-        <p class="subtitle">Frontend conectado con backend-saludo </p>
+        <h1>Prueba Saludo Implementacion 1</h1>
+        <p class="subtitle">Frontend conectado con backend-saludo 1</p>
 
         <div class="buttons">
           <button type="button" (click)="cargarSaludo()">GET /api/saludo</button>
